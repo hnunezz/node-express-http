ac35847cac4b81f96ba93c872fbdfc31a955ee25
hnunezz <nunesdealmeidahenrique@gmail.com>
2023-10-10 15:07:59 -0300

    Feat: add os information and ram usage

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const http = require("http");
 
 const userRoute = require("./routes/userRoutes");
+const statsRoute = require("./pcRamUsage.js");
 
 const app = express();
+const host = "http://localhost:";
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 userRoute(app);
 
-app.get("/", (req, res) => res.send("Hello World in Express!"));
+app.get("/", (req, res) => {
+  res.send("Hello World in Express!");
+});
 
-app.listen(port, () => console.log("Api Running in localhost:3000"));
+app.get("/stats", (req, res) => {
+  res.send(statsRoute);
+});
+
+app.listen(port, () => console.log(`Server Running in ${host}${port}`));
